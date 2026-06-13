97ed9e82082fa924ca8602addd132aba5a358875
soumyajit-18-shipi-it <routsoumyajit18@gmail.com>
2026-06-13 22:35:04 +0530

    fix: __init__.py conflict

--- backend/ml/__init__.py
+++ backend/ml/__init__.py
@@ -1,11 +1,11 @@
 from ml.base_model import BaseModel
 from ml.linear_model import LinearRegressionModel
 from ml.predictor import StockPredictor
-<<<<<<< HEAD
+
 from ml.ensemble import arbitrate, EnsembleResult
-=======
+
 from ml.random_forest_model import RandomForestModel
->>>>>>> 43c89386f948b8a790430e72f627b7b9a714bb65
+
 
 __all__ = [
     "BaseModel",
